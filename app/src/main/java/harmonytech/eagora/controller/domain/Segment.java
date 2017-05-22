74567796bf0a57305b2ed1d1f45187c687c08872
César Rodrigues <rasec1287@hotmail.com>
2017-05-22 13:41:08 -0300

    Adicionada RecyclerView app

---
+++ app/src/main/java/harmonytech/eagora/controller/domain/Segment.java
@@ -0,0 +1,22 @@
+package harmonytech.eagora.controller.domain;
+
+public class Segment {
+
+    private String name, banner;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBanner() {
+        return banner;
+    }
+
+    public void setBanner(String banner) {
+        this.banner = banner;
+    }
+}
---
+++ app/src/main/java/harmonytech/eagora/view/MainActivity.java
@@ -0,0 +1,46 @@
+package harmonytech.eagora.view;
+
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import harmonytech.eagora.R;
+import harmonytech.eagora.controller.domain.Segment;
+import harmonytech.eagora.controller.fragment.SegmentFragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    ArrayList<Segment> recipesList = new ArrayList<>();
+    SegmentFragment frag;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Segment s1 = new Segment();
+        s1.setName("Serviços");
+        s1.setBanner("http://www.eleveninstrumentacao.com.br/ESW/Images/servicos.jpg");
+        recipesList.add(s1);
+
+        Segment s2 = new Segment();
+        s2.setName("Metalúrgica");
+        s2.setBanner("http://i.imgur.com/7unwhF0.jpg");
+        recipesList.add(s2);
+
+        frag = (SegmentFragment) getSupportFragmentManager().findFragmentByTag("mainFrag");
+        if(frag == null) {
+            frag = new SegmentFragment();
+            FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+            ft.replace(R.id.segments_fragment_container, frag, "mainFrag");
+            ft.commit();
+        }
+    }
+
+    public List<Segment> getRecipesList() {
+        return recipesList;
+    }
+}

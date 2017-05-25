5606bb4ef1a1ad8a68a253fb5f2d0101c6ef1f9c
César Rodrigues <rasec1287@hotmail.com>
2017-05-25 11:47:51 -0300

    Adicionada tela de detalhe dos segmentos

---
+++ app/src/main/java/harmonytech/eagora/view/SegmentDetailsActivity.java
@@ -0,0 +1,15 @@
+package harmonytech.eagora.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import harmonytech.eagora.R;
+
+public class SegmentDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_segment_details);
+    }
+}

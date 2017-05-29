4f2e9d04655112d930ccd9f4cd25bb83cf2e20bd
Islane <islane.junior@gmail.com>
2017-05-29 10:13:14 -0300

    Melhoria no layout, centralização dos nomes das categorias

--- app/src/main/java/harmonytech/eagora/view/MainActivity.java
+++ app/src/main/java/harmonytech/eagora/view/MainActivity.java
@@ -3,10 +3,8 @@ package harmonytech.eagora.view;
 import android.media.MediaPlayer;
 import android.net.Uri;
 import android.support.v4.app.FragmentTransaction;
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
 import android.widget.VideoView;
 
 import java.util.ArrayList;

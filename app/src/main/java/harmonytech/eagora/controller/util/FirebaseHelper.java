c972250e45aabdc5e5a9c50813b83fbb42cd224d
César Rodrigues <rasec1287@hotmail.com>
2017-05-29 15:22:05 -0300

    Adicionada classe para Strings estáticas do Firebase

---
+++ app/src/main/java/harmonytech/eagora/controller/util/FirebaseHelper.java
@@ -0,0 +1,12 @@
+package harmonytech.eagora.controller.util;
+
+public class FirebaseHelper {
+
+    public static final String FIREBASE_DATABASE_PROVIDER_NAME = "nome";
+    public static final String FIREBASE_DATABASE_PROVIDER_EMAIL = "email";
+    public static final String FIREBASE_DATABASE_PROVIDER_PHONE = "telefone";
+    public static final String FIREBASE_DATABASE_PROVIDER_POSTAL_CODE = "cep";
+    public static final String FIREBASE_DATABASE_PROVIDER_BIRTH = "nascimento";
+    public static final String FIREBASE_DATABASE_PROVIDER_CATEGORY = "categoria";
+    public static final String FIREBASE_DATABASE_PROVIDER_SUBCATEGORY = "subcategoria";
+}

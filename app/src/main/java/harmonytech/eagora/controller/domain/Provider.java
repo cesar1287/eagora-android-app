b4ae7186a33ff0634938f7a7afab784be0711632
César Rodrigues <rasec1287@hotmail.com>
2017-05-29 15:21:31 -0300

    Adicionada classe para os provedores

---
+++ app/src/main/java/harmonytech/eagora/controller/domain/Provider.java
@@ -0,0 +1,54 @@
+package harmonytech.eagora.controller.domain;
+
+public class Provider {
+
+    private String name, email, phone, postalCode, birth, subcategory;
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
+
+    public String getBirth() {
+        return birth;
+    }
+
+    public void setBirth(String birth) {
+        this.birth = birth;
+    }
+
+    public String getSubcategory() {
+        return subcategory;
+    }
+
+    public void setSubcategory(String subcategory) {
+        this.subcategory = subcategory;
+    }
+}

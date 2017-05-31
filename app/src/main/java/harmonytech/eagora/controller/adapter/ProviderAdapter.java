387759832b9bb0dd1ae722bedc003c026a1159ef
César Rodrigues <rasec1287@hotmail.com>
2017-05-31 13:26:12 -0300

    Adicionado RecyclerView para as infos dos prestadores

---
+++ app/src/main/java/harmonytech/eagora/controller/adapter/ProviderAdapter.java
@@ -0,0 +1,8 @@
+package harmonytech.eagora.controller.adapter;
+
+/**
+ * Created by CACOMP on 31/05/2017.
+ */
+
+public class ProviderAdapter {
+}
---
+++ app/src/main/java/harmonytech/eagora/controller/fragment/ProviderFragment.java
@@ -0,0 +1,8 @@
+package harmonytech.eagora.controller.fragment;
+
+/**
+ * Created by CACOMP on 31/05/2017.
+ */
+
+public class ProviderFragment {
+}

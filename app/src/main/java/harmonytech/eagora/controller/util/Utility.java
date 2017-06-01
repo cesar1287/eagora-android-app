b8b94dad95ed70aed475d622445b5dd190ccb0fd
César Rodrigues <rasec1287@hotmail.com>
2017-06-01 11:47:10 -0300

    Adicionado teste e snackBar quando não se tem conexão

--- app/src/main/java/harmonytech/eagora/controller/util/Utility.java
+++ app/src/main/java/harmonytech/eagora/controller/util/Utility.java
@@ -1,5 +1,18 @@
 package harmonytech.eagora.controller.util;
 
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.Color;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.TextView;
+
+import com.androidadvance.topsnackbar.TSnackbar;
+
+import harmonytech.eagora.R;
+
 public class Utility {
 
     public static final String URI_PACKAGE = "android.resource://harmonytech.eagora/";
@@ -25,4 +38,23 @@ public class Utility {
     public static final String HASH_MAP_FIREBASE = "firebase";
 
     public static final String SEGMENT_DETAILS_TITLE = "title";
+
+    public static boolean isConnected(Activity activity){
+        ConnectivityManager cm =
+                (ConnectivityManager)activity.getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
+        return activeNetwork != null && activeNetwork.isConnectedOrConnecting();
+    }
+
+    public static void showSnackBarErrorMessage(Activity activity, String errorMessage){
+
+        TSnackbar snackbar = TSnackbar.make(activity.findViewById(android.R.id.content), errorMessage, TSnackbar.LENGTH_SHORT);
+        View snackbarView = snackbar.getView();
+        snackbarView.setBackgroundColor(activity.getResources().getColor(R.color.redNoInternetConnection));
+        TextView textView = (TextView) snackbarView.findViewById(com.androidadvance.topsnackbar.R.id.snackbar_text);
+        textView.setTextColor(Color.WHITE);
+        textView.setGravity(Gravity.CENTER_HORIZONTAL);
+        snackbar.show();
+    }
 }
--- app/src/main/java/harmonytech/eagora/view/SegmentCategoryActivity.java
+++ app/src/main/java/harmonytech/eagora/view/SegmentCategoryActivity.java
@@ -1,14 +1,20 @@
 package harmonytech.eagora.view;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.Toast;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.androidadvance.topsnackbar.TSnackbar;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -24,10 +30,14 @@ public class SegmentCategoryActivity extends AppCompatActivity implements Adapte
     String segmento, segmentoFirebase;
     HashMap<String, ArrayList<String>> arraySegmento;
 
+    RelativeLayout relativeLayout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_segment_category);
+
+        relativeLayout = (RelativeLayout) findViewById(R.id.relativeLayout);
 
         segmento = getIntent().getStringExtra(Utility.SEGMENTO);
 
@@ -43,11 +53,15 @@ public class SegmentCategoryActivity extends AppCompatActivity implements Adapte
     @Override
     public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
 
-        Intent intent = new Intent(this, SegmentDetailsActivity.class);
-        intent.putExtra(FirebaseHelper.FIREBASE_DATABASE_PROVIDER_CATEGORY, segmentoFirebase);
-        intent.putExtra(FirebaseHelper.FIREBASE_DATABASE_PROVIDER_SUBCATEGORY, arraySegmento.get(Utility.HASH_MAP_FIREBASE).get(i));
-        intent.putExtra(Utility.SEGMENT_DETAILS_TITLE, arraySegmento.get(Utility.HASH_MAP_TELA).get(i));
-        startActivity(intent);
+        if(Utility.isConnected(this)){
+            Intent intent = new Intent(this, SegmentDetailsActivity.class);
+            intent.putExtra(FirebaseHelper.FIREBASE_DATABASE_PROVIDER_CATEGORY, segmentoFirebase);
+            intent.putExtra(FirebaseHelper.FIREBASE_DATABASE_PROVIDER_SUBCATEGORY, arraySegmento.get(Utility.HASH_MAP_FIREBASE).get(i));
+            intent.putExtra(Utility.SEGMENT_DETAILS_TITLE, arraySegmento.get(Utility.HASH_MAP_TELA).get(i));
+            startActivity(intent);
+        }else{
+            Utility.showSnackBarErrorMessage(this, getResources().getString(R.string.no_internet_connection));
+        }
     }
 
     private void setupUI() {

55f94b880def7f35e47ad83538d9003516b37025
César Rodrigues <rasec1287@hotmail.com>
2017-06-04 23:29:09 -0300

    Implementada parcialmente tela de FAQ

--- app/src/main/java/harmonytech/eagora/view/DoubtActivity.java
+++ app/src/main/java/harmonytech/eagora/view/DoubtActivity.java
@@ -1,20 +1,44 @@
 package harmonytech.eagora.view;
 
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.MenuItem;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
+import android.widget.ExpandableListAdapter;
+import android.widget.ExpandableListView;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 import harmonytech.eagora.R;
-import harmonytech.eagora.controller.util.Utility;
 
 public class DoubtActivity extends AppCompatActivity {
+
+    ExpandableListAdapter listAdapter;
+    ExpandableListView expListView;
+
+    List<String> listDataHeader;
+    HashMap<String, List<String>> listDataChild;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_doubt);
+
+        // get the listview
+        expListView = (ExpandableListView) findViewById(R.id.lvExp);
+
+        listDataHeader = new ArrayList<>();
+        listDataChild = new HashMap<>();
+
+        listDataHeader.add(getResources().getString(R.string.question_1));
+        List<String> answer = new ArrayList<>();
+        answer.add(getResources().getString(R.string.answer_1));
+
+        listDataChild.put(getResources().getString(R.string.question_1), answer);
+
+        listAdapter = new harmonytech.eagora.controller.util.ExpandableListAdapter(DoubtActivity.this, listDataHeader, listDataChild);
+
+        // setting list adapter
+        expListView.setAdapter(listAdapter);
     }
 }
